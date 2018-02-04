eb40722c147e2d2a897c11b8271f9cd7b956b6c6
nelsondavenapalli <nelson.csits@gmail.com>
2018-02-04 16:43:31 +0100

    Adding code to create CloseableHttpClient where nohostname verifier strategy is used and untrusted certs are used.

--- MultiPartFormDataRequestCreaterClient/src/main/java/fun/abm/com/MultiPartFormDataRequestCreaterClient/MultipPartFormDataRequestCreaterClientRunner.java
+++ MultiPartFormDataRequestCreaterClient/src/main/java/fun/abm/com/MultiPartFormDataRequestCreaterClient/MultipPartFormDataRequestCreaterClientRunner.java
@@ -1,6 +1,97 @@
 package fun.abm.com.MultiPartFormDataRequestCreaterClient;
 
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.cache.CacheConfig;
+import org.apache.http.impl.client.cache.CachingHttpClientBuilder;
+import org.apache.http.impl.client.cache.ExponentialBackOffSchedulingStrategy;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
 public class MultipPartFormDataRequestCreaterClientRunner {
+
+    @Value("${img.importurl}")
+    String solrImportUrl;
+
+    private CloseableHttpClient httpClient;
+
+    @Bean
+    public CloseableHttpClient getHttpClient() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        SSLContext sslContext = new SSLContextBuilder()
+                .loadTrustMaterial(null, (certificate, authType) -> true).build();
+
+        CachingHttpClientBuilder hcb = CachingHttpClientBuilder.create();
+        CacheConfig cc = CacheConfig.DEFAULT;
+        ExponentialBackOffSchedulingStrategy ebo = new ExponentialBackOffSchedulingStrategy(cc, 2, 2000, 30000);
+        hcb.setSchedulingStrategy(ebo);
+
+        CloseableHttpClient client = hcb
+                .setSSLContext(sslContext)
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .setRetryHandler((exception, executionCount, context) -> {
+                    if (executionCount < 10)
+                        return true;
+                    return false;
+                })
+                .build();
+
+        return client;
+    }
+
+    public CloseableHttpClient createHttpClient_AcceptsUntrustedCerts() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        HttpClientBuilder b = HttpClientBuilder.create();
+
+        // setup a Trust Strategy that allows all certificates.
+        //
+        SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
+            public boolean isTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                return true;
+            }
+        }).build();
+        b.setSslcontext(sslContext);
+
+        // don't check Hostnames, either.
+        //      -- use SSLConnectionSocketFactory.getDefaultHostnameVerifier(), if you don't want to weaken
+        HostnameVerifier hostnameVerifier = SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+
+        // here's the special part:
+        //      -- need to create an SSL Socket Factory, to use our weakened "trust strategy";
+        //      -- and create a Registry, to register it.
+        //
+        SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext, hostnameVerifier);
+        org.apache.http.config.Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                .register("https", sslSocketFactory)
+                .build();
+
+        // now, we create connection-manager using our Registry.
+        //      -- allows multi-threaded use
+        PoolingHttpClientConnectionManager connMgr = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+        b.setConnectionManager(connMgr);
+
+        // finally, build the HttpClient;
+        //      -- done!
+        CloseableHttpClient client = b.build();
+        return client;
+    }
+
+
     public void postMultiFormDataToLocalhost() {
 
 
